0924502baacea051ebd4282e978719f013f61006
euhansarkar <euhansarkar@gmail.com>
2025-03-31 23:26:14 +0600

    Added: Tuple.

---
+++ tuples.py
@@ -0,0 +1,23 @@
+thistuple = ("hello", "world", "what", "are", "you", "doing")
+
+
+# print(thistuple)
+
+tuple1 = ("hello", "world", "what", "are", "you", "you", "world")
+
+# print(tuple1[1:3])
+
+# print(tuple1[4:])
+
+# print("see length", len(tuple1))
+
+tuple2 = ("hello")
+
+# if tuple creates with only 1 item then it's by default string
+# print(type(tuple2))
+
+
+
+tuple3 = ("hello",)
+# if tuple creates with more then 1 item then it's tuple
+print(type(tuple3))
